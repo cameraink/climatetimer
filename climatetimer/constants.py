74cdb4542cde65c1c31c01d57e1c00fda3361901
Guillaume Dion <guillaume.dion@qiti.eu>
2025-02-21 11:16:31 +0100

    Added info description as constants

--- climatetimer/constants.py
+++ climatetimer/constants.py
@@ -7,6 +7,18 @@ REFERENCES = {
     "kyoto": datetime(2005, 2, 16, 0, 0, 0, tzinfo=timezone.utc),
 }
 
+# Descriptions for each reference event
+REFERENCE_INFO = {
+    "paris": (
+        "Paris Agreement (April 22, 2016): Global commitment to limit warming to well below "
+        "2°C above pre-industrial levels with 190+ countries participating."
+    ),
+    "kyoto": (
+        "Kyoto Protocol (February 16, 2005): First binding international agreement to reduce "
+        "greenhouse gas emissions with 192 countries participating."
+    ),
+}
+
 # Fixed duration constants in seconds
 SECOND_DURATION = 1
 MINUTE_DURATION = 60
